005723186a2d1f7002f1b3bf3a4566091529bc45
Avishai Carmel <acarmel@lightricks.com>
2025-07-24 15:06:58 +0300

    a

--- my_playground/openai_exmaple.py
+++ my_playground/openai_exmaple.py
@@ -1,6 +1,13 @@
 import base64
 from openai import OpenAI
 
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
 client = OpenAI(
     base_url="http://localhost:8888/v1",
     api_key="abc",
@@ -8,15 +15,21 @@ client = OpenAI(
 
 
 
+
 system_prompt = open("my_playground/system_prompt.txt", "r").read()
 user_prompt = open("my_playground/user_prompt.txt", "r").read()
-image_b64 = base64.b64encode(open("my_playground/01.png", "rb").read()).decode("utf-8")
+image_b64 = encode_image("my_playground/01.png")
 
 messages = [
     {"role": "system", "content": system_prompt},
     {"role": "user", "content": [
         {"type": "text", "text": user_prompt},
-        {"type": "image", "image": image_b64}
+        {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{image_b64}"
+            }
+        }
     ]}
 ]
 
@@ -26,3 +39,4 @@ completion = client.chat.completions.create(
     messages=messages,
 )
 
+print(completion.choices[0].message.content)
